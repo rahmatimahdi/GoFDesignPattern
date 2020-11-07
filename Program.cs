9dbb374977e7d1f20b7a52eb89586b3ff7d97bf6
rahmatimahdi <rahmatymahdi@live.com>
2020-11-07 20:07:24 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GoFDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton.Singleton.UniqueInstance.Test();
+            new Decorator.decorator();
+        }
+    }
+}
